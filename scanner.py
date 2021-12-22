7785a01b549b6e14ccf03cfc9bcb4b6c2b6c1fb7
aaaakshat <33050725+aaaakshat@users.noreply.github.com>
2021-12-21 21:17:18 -0500

    Add uuid

--- scanner.py
+++ scanner.py
@@ -3,6 +3,8 @@
 import asyncio
 from bleak import BleakScanner
 
+uuid = "251385E1-E5B1-A22A-70C1-BC056642B5B9"
+
 async def main():
     devices = await BleakScanner.discover()
     for d in devices:
